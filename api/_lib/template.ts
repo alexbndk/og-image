6e3b4b014a5d5a5ad04f0e25f0581c9e5da04113
Alex Bendiken <alex@bndk.co>
2021-07-29 00:48:26 +0200

    Customize image template

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,92 +1,39 @@
-
-import { readFileSync } from 'fs';
-import marked from 'marked';
-import { sanitizeHtml } from './sanitizer';
 import { ParsedRequest } from './types';
+import marked from 'marked';
+import { readFileSync } from 'fs';
+import { sanitizeHtml } from './sanitizer';
 const twemoji = require('twemoji');
 const twOptions = { folder: 'svg', ext: '.svg' };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
 
-const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
-const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
+const rglr = readFileSync(`${__dirname}/../_fonts/Hellix-Regular.woff2`).toString('base64');
+const bold = readFileSync(`${__dirname}/../_fonts/Hellix-Bold.woff2`).toString('base64');
+const aurora = readFileSync(`${__dirname}/../_images/aurora.png`).toString('base64');
+const logo = readFileSync(`${__dirname}/../_images/logo.png`).toString('base64');
 
-function getCss(theme: string, fontSize: string) {
-    let background = 'white';
-    let foreground = 'black';
-    let radial = 'lightgray';
-
-    if (theme === 'dark') {
-        background = 'black';
-        foreground = 'white';
-        radial = 'dimgray';
-    }
+function getCss() {
     return `
     @font-face {
-        font-family: 'Inter';
+        font-family: 'Hellix';
         font-style:  normal;
         font-weight: normal;
         src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
     }
 
     @font-face {
-        font-family: 'Inter';
+        font-family: 'Hellix';
         font-style:  normal;
         font-weight: bold;
         src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
     }
 
-    @font-face {
-        font-family: 'Vera';
-        font-style: normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
-
     body {
-        background: ${background};
-        background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
-        background-size: 100px 100px;
+        background: #041417 url(data:image/png;charset=utf-8;base64,${aurora}) no-repeat 150% 75%;
+        background-size: 60%;
         height: 100vh;
-        display: flex;
-        text-align: center;
-        align-items: center;
-        justify-content: center;
+        padding: 100px;
     }
-
-    code {
-        color: #D400FF;
-        font-family: 'Vera';
-        white-space: pre-wrap;
-        letter-spacing: -5px;
-    }
-
-    code:before, code:after {
-        content: '\`';
-    }
-
-    .logo-wrapper {
-        display: flex;
-        align-items: center;
-        align-content: center;
-        justify-content: center;
-        justify-items: center;
-    }
-
-    .logo {
-        margin: 0 75px;
-    }
-
-    .plus {
-        color: #BBB;
-        font-family: Times New Roman, Verdana;
-        font-size: 100px;
-    }
-
-    .spacer {
-        margin: 150px;
-    }
-
+    
     .emoji {
         height: 1em;
         width: 1em;
@@ -95,52 +42,43 @@ function getCss(theme: string, fontSize: string) {
     }
     
     .heading {
-        font-family: 'Inter', sans-serif;
-        font-size: ${sanitizeHtml(fontSize)};
+        margin-top: 75px;
+        width: 66%;
+        font-family: 'Hellix', sans-serif;
+        font-size: 120px;
         font-style: normal;
-        color: ${foreground};
-        line-height: 1.8;
+        font-weight: bold;
+        color: #fff;
+        line-height: 1.25;
+    }
+    
+    .subtitle {
+        margin-top: 50px;
+        width: 66%;
+        font-family: 'Hellix', sans-serif;
+        font-size: 65px;
+        font-style: normal;
+        color: #fff9;
+        line-height: 1.33;
     }`;
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
-    const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
+    const { text, subtitle } = parsedReq;
     return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
     <title>Generated Image</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
-        ${getCss(theme, fontSize)}
+        ${getCss()}
     </style>
     <body>
         <div>
-            <div class="spacer">
-            <div class="logo-wrapper">
-                ${images.map((img, i) =>
-                    getPlusSign(i) + getImage(img, widths[i], heights[i])
-                ).join('')}
-            </div>
-            <div class="spacer">
-            <div class="heading">${emojify(
-                md ? marked(text) : sanitizeHtml(text)
-            )}
-            </div>
+            <img src="data:image/png;charset=utf-8;base64,${logo}" alt="Aurora" />
+            <div class="heading">${emojify(sanitizeHtml(text))}</div>
+            <div class="subtitle">${emojify(sanitizeHtml(subtitle))}</div>
         </div>
     </body>
 </html>`;
-}
-
-function getImage(src: string, width ='auto', height = '225') {
-    return `<img
-        class="logo"
-        alt="Generated Image"
-        src="${sanitizeHtml(src)}"
-        width="${sanitizeHtml(width)}"
-        height="${sanitizeHtml(height)}"
-    />`
-}
-
-function getPlusSign(i: number) {
-    return i === 0 ? '' : '<div class="plus">+</div>';
 }
